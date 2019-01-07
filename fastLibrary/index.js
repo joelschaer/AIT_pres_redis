299d6d0f56364459fc7063ae19024600fd5a78c6
zutt <yannlederrey@gmail.com>
2019-01-07 11:26:50 +0100

    bite.js

--- fastLibrary/index.js
+++ fastLibrary/index.js
@@ -3,8 +3,31 @@ var express = require('express'),
     app = express(),
     mongoUrl = 'mongodb+srv://ait:ait@cluster0-bj3cx.mongodb.net/test?retryWrites=true';
 
+var access = require('./access.js');
+
 MongoClient.connect(mongoUrl, function (err, db) {
     if (err) throw 'Error connecting to database - ' + err;
+
+    app.post('/book', function (req, res) {
+        if (!req.body.title || !req.body.author) res.status(400).send("Please send a title and an author for the book");
+        else if (!req.body.text) res.status(400).send("Please send some text for the book");
+        else {
+            access.saveBook(db, req.body.title, req.body.author, req.body.text, function (err) {
+                if (err) res.status(500).send("Server error");
+                else res.status(201).send("Saved");
+            });
+        }
+    });
+
+    app.get('/book/:title', function (req, res) {
+        if (!req.param('title')) res.status(400).send("Please send a proper title");
+        else {
+            access.findBookByTitle(db, req.param('title'), function (book) {
+                if (!text) res.status(500).send("Server error");
+                else res.status(200).send(book);
+            });
+        }
+    });
 
     app.listen(8000, function () {
         console.log('Listening on port 8000');
